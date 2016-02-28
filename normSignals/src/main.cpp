f5af7417fd6fb1299358660fb9126400f6b7f912
Michael Purcaro <purcaro@gmail.com>
2016-02-28 16:44:28 -0500

    refactor

--- normSignals/src/main.cpp
+++ normSignals/src/main.cpp
@@ -15,6 +15,7 @@
 
 #include <zi/zargs/zargs.hpp>
 ZiARG_string(assembly, "", "assembly");
+ZiARG_string(chrLenFnp, "", "chrom lengths file");
 ZiARG_bool(debug, false, "debug");
 
 #include "cpp/files.hpp"
@@ -26,34 +27,32 @@ namespace bib {
 namespace a = arma;
 
 class Norm{
-    bfs::path inFnp_;
+    const bfs::path inFnp_;
+    const std::string assembly_;
+    const bfs::path chrLenFnp_;
 
-public:
-    Norm(std::string inFnp)
-        : inFnp_(inFnp)
-    {}
+    std::map<std::string, std::vector<zentlib::Interval>> bwData_;
+    uint64_t totalBases_ = 0;
+    float mean_ = 0;
+    float stddev_ = 0;
 
-    void run(){
-        if(!bfs::exists(inFnp_)){
-            throw std::runtime_error("ERROR: file missing: " +
-                                     inFnp_.string());
-        }
-
-        std::map<std::string, std::vector<zentlib::Interval>> bwData;
-
+    void loadData(){
         std::cout << "loading data..." << std::endl;
+
         zentlib::BigWig bw(inFnp_);
-        uint64_t totalBases = 0;
         for(const auto& chrAndSize : bw.ChromsAndSizes()){
             auto chr = chrAndSize.first;
-            totalBases += chrAndSize.second + 1;
-            bwData[chr] = bw.Data(chr);
+            totalBases_ += chrAndSize.second + 1;
+            bwData_[chr] = bw.Data(chr);
         }
+    }
 
+    auto combineAll(){
         std::cout << "combining all values..." << std::endl;
+
         std::vector<float> values;
-        values.reserve(totalBases);
-        for(const auto& chrAndData : bwData){
+        values.reserve(totalBases_);
+        for(const auto& chrAndData : bwData_){
             auto& intervals = chrAndData.second;
             for(size_t i = 0; i < intervals.size(); ++i){
                 const auto& d = intervals[i];
@@ -64,40 +63,41 @@ public:
                     values.push_back(d.val);
                 }
             }
-            std::cout << "\t" << chrAndData.first << " "
-                      << values.size() << "\n";
+            //std::cout << "\t" << chrAndData.first << " " << values.size() << "\n";
         }
 
-        // z-score normalize
         std::cout << "computing mean/stddev..." << std::endl;
         a::fvec av(values.data(), values.size(), false, true);
-        const float mean = a::mean(a::vectorise(av));
-        const float stddev = a::stddev(a::vectorise(av));
+        mean_ = a::mean(av);
+        stddev_ = a::stddev(av);
+    }
 
+    void normalize(){
+        // z-score normalize
         std::cout << "transforming..." << std::endl;
-        for(auto& chrAndData : bwData){
+        for(auto& chrAndData : bwData_){
             auto& intervals = chrAndData.second;
-            std::cout << "\t" << chrAndData.first << std::endl;
+            //std::cout << "\t" << chrAndData.first << std::endl;
             #pragma omp parallel for
             for(size_t i = 0; i < intervals.size(); ++i){
                 auto& d = intervals[i];
-                d.val = (d.val - mean) / stddev;
+                d.val = (d.val - mean_) / stddev_;
                 d.val = clamp(d.val, -1.96, 1.96); // ignore extremes
                 d.val += 1.96; // shift up
             }
         }
+    }
 
+    void writeBed(bfs::path outFnp){
         std::cout << "writing transformed bed..." << std::endl;
-        bfs::path outFnp = inFnp_.replace_extension(".norm.bed");
-        outFnp = str::replace(outFnp.string(), "encode/data/", "encode/norm");
-        files::ensureDir(outFnp);
+
         std::ofstream out(outFnp.string());
         if(!out.is_open()){
             throw std::runtime_error("could not open file " +
                                      outFnp.string());
         }
-        for(auto& chrAndData : bwData){
-            std::cout << "\t" << chrAndData.first << std::endl;
+        for(auto& chrAndData : bwData_){
+            //std::cout << "\t" << chrAndData.first << std::endl;
             for(auto& d : chrAndData.second){
                 if(0 == d.val){
                     continue;
@@ -111,6 +111,35 @@ public:
         out.close();
         std::cout << "wrote " << outFnp << std::endl;
     }
+
+public:
+    Norm(std::string inFnp, std::string assembly, std::string chrLenFnp)
+        : inFnp_(inFnp)
+        , assembly_(assembly)
+        , chrLenFnp_(chrLenFnp)
+    {}
+
+    void run(){
+        if(!bfs::exists(inFnp_)){
+            throw std::runtime_error("ERROR: file missing: " +
+                                     inFnp_.string());
+        }
+
+        loadData();
+        combineAll();
+        normalize();
+
+        bfs::path outFnp = str::replace(inFnp_.string(),
+                                        "encode/data/", "encode/norm");
+        outFnp.replace_extension(".norm.bed");
+        files::ensureDir(outFnp);
+
+        writeBed(outFnp);
+
+        bfs::path bwFnp = outFnp;
+        bwFnp.replace_extension(".bigWig");
+        //BigWig::BedToBigWig(outFnp, bwFnp, chromLenFnp_);
+    }
 };
 
 } // namespace bib
@@ -119,7 +148,7 @@ int main(int argc, char** argv){
     zi::parse_arguments(argc, argv, true);  // modifies argc and argv
     const auto args = std::vector<std::string>(argv + 1, argv + argc);
 
-    bib::Norm n(args.at(0));
+    bib::Norm n(args.at(0), ZiARG_assembly, ZiARG_chrLenFnp);
     n.run();
 
     return 0;
