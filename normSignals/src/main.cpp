8f97a8aba20b37cc7f412e1f13426a6ab071e77e
Michael Purcaro <purcaro@gmail.com>
2016-02-27 19:31:27 -0500

    print out steps; rename output folder; bug fix for shift up

--- normSignals/src/main.cpp
+++ normSignals/src/main.cpp
@@ -11,7 +11,7 @@
 
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
-#include<json/writer.h>
+#include <json/writer.h>
 
 #include <zi/zargs/zargs.hpp>
 ZiARG_string(assembly, "", "assembly");
@@ -41,15 +41,22 @@ public:
 
         std::map<std::string, std::vector<zentlib::Interval>> bwData;
 
+        std::cout << "loading data..." << std::endl;
         zentlib::BigWig bw(inFnp_);
+        uint64_t totalBases = 0;
         for(const auto& chrAndSize : bw.ChromsAndSizes()){
             auto chr = chrAndSize.first;
+            totalBases += chrAndSize.second + 1;
             bwData[chr] = bw.Data(chr);
         }
 
-        std::vector<double> values;
+        std::cout << "combining all values..." << std::endl;
+        std::vector<float> values;
+        values.reserve(totalBases);
         for(const auto& chrAndData : bwData){
-            for(const auto& d : chrAndData.second){
+            auto& intervals = chrAndData.second;
+            for(size_t i = 0; i < intervals.size(); ++i){
+                const auto& d = intervals[i];
                 for (uint32_t i = d.start; i < d.end; ++i) {
                     if(0){
                         // check unmappable....
@@ -57,29 +64,44 @@ public:
                     values.push_back(d.val);
                 }
             }
+            std::cout << "\t" << chrAndData.first << " "
+                      << values.size() << "\n";
         }
 
         // z-score normalize
-        a::vec av(values.data(), values.size(), false, true);
-        const double mean = a::mean(a::vectorise(av));
-        const double stddev = a::stddev(a::vectorise(av));
+        std::cout << "computing mean/stddev..." << std::endl;
+        a::fvec av(values.data(), values.size(), false, true);
+        const float mean = a::mean(a::vectorise(av));
+        const float stddev = a::stddev(a::vectorise(av));
 
+        std::cout << "transforming..." << std::endl;
         for(auto& chrAndData : bwData){
-            for(auto& d : chrAndData.second){
+            auto& intervals = chrAndData.second;
+            std::cout << "\t" << chrAndData.first << std::endl;
+            #pragma omp parallel for
+            for(size_t i = 0; i < intervals.size(); ++i){
+                auto& d = intervals[i];
                 d.val = (d.val - mean) / stddev;
                 d.val = clamp(d.val, -1.96, 1.96); // ignore extremes
-                d.val += -1.96; // shift up
+                d.val += 1.96; // shift up
             }
         }
 
+        std::cout << "writing transformed bed..." << std::endl;
         bfs::path outFnp = inFnp_.replace_extension(".norm.bed");
+        outFnp = str::replace(outFnp.string(), "encode/data/", "encode/norm");
+        files::ensureDir(outFnp);
         std::ofstream out(outFnp.string());
         if(!out.is_open()){
             throw std::runtime_error("could not open file " +
                                      outFnp.string());
         }
         for(auto& chrAndData : bwData){
+            std::cout << "\t" << chrAndData.first << std::endl;
             for(auto& d : chrAndData.second){
+                if(0 == d.val){
+                    continue;
+                }
                 out << chrAndData.first << "\t"
                     << d.start << "\t"
                     << d.end << "\t"
@@ -87,6 +109,7 @@ public:
             }
         }
         out.close();
+        std::cout << "wrote " << outFnp << std::endl;
     }
 };
 
