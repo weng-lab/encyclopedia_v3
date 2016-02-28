1d9647ae43b2e2a4129420e3dfbe838786dd945d
Michael Purcaro <purcaro@gmail.com>
2016-02-28 17:46:10 -0500

    rename to python; renmae output bin

--- normSignals/normBigWigs.py
+++ normSignals/normBigWigs.py
@@ -2,6 +2,7 @@
 
 import os, sys, json, argparse
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../website/'))
 from web_epigenomes import WebEpigenomesLoader
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils/'))
@@ -9,8 +10,22 @@ from utils import Utils, printWroteNumLines
 from metadataws import MetadataWS
 from files_and_paths import Datasets
 
+def process(args, exp):
+    try:
+        bigWigFnp, bedAssembly = exp.getSingleBigWigSingleFnp(args)
+        if not bigWigFnp:
+            print exp.getSingleBigWigSingleFnp(args)
+            print "missing", exp
+        else:
+            cmds = ["
+            fnps.append(bigWigFnp)
+    except Exception, e:
+        return "bad " + str(e)
+
 def build(args):
-    fnps = []
+    jr = JobRunner(scriptFnp = os.path.realpath(__file__),
+                   jobType = PythonJob,
+                   cpus = args.j)
 
     epigenomes = WebEpigenomesLoader(args)
     for assembly in ["hg19", "mm10", "mm9"]:
@@ -18,22 +33,19 @@ def build(args):
             epis = epigenomes.GetByAssemblyAndAssays(assembly, assays)
             for epi in epis.epis:
                 for exp in epi.exps():
-                    try:
-                        bigWigFnp, bedAssembly = exp.getSingleBigWigSingleFnp(args)
-                        if not bigWigFnp:
-                            print exp.getSingleBigWigSingleFnp(args)
-                            print "missing", exp
-                        else:
-                            fnps.append(bigWigFnp)
-                    except Exception, e:
-                        print str(e)
-                        print "bad exp:", exp
+                    jr.append([[args, exps.append(exp)]])
+
+    if args.test:
+        return jr.runOne(process)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--process', action="store_true", default=True)
     parser.add_argument('--local', action="store_true", default=False)
     parser.add_argument('--rebuild', action="store_true", default=False)
+    parser.add_argument('--test', action="store_true", default=False)
+    parser.add_argument('--job', type=int, default=0)
+    parser.add_argument('-j', type=int, default=4)
     args = parser.parse_args()
     return args
 
