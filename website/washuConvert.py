6242fe7c4b227b36b69b587aac1cd4585db827bc
Michael Purcaro <purcaro@gmail.com>
2016-03-07 19:36:38 -0500

    convert bed for washu

--- website/washuConvert.py
+++ website/washuConvert.py
@@ -3,7 +3,7 @@
 import os, sys, shutil
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils'))
-from utils import Utils
+from utils import Utils, printWroteNumLines
 from files_and_paths import Dirs
 
 def main():
@@ -19,7 +19,13 @@ def main():
         print(inFnp)
 
         outFnp = os.path.join(outD, fn)
-        shutil.copyfile(inFnp, outFnp)
+        with open(inFnp) as inF:
+            with open(outFnp, 'w') as outF:
+                for idx, line in enumerate(inF):
+                    toks = line.rstrip().split('\t')
+                    toks + [str(idx), '.']
+                    outF.write("\t".join(toks) + '\n')
+        printWroteNumLines(outFnp)
 
         cmds = ["bgzip", '-f', outFnp]
         Utils.runCmds(cmds)
