cb76c5f546e99c7bff3d859beea3dbd08d85008c
Michael Purcaro <purcaro@gmail.com>
2016-03-07 21:37:38 -0500

    tweaks for hammock; parallel

--- website/washuConvert.py
+++ website/washuConvert.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 
 import os, sys, shutil
+from joblib import Parallel, delayed
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils'))
 from utils import Utils, printWroteNumLines
 from files_and_paths import Dirs
+
+def job(args):
+    fn = args[0]
+    inFnp = args[1]
+    outD = args[2]
+
+    outFnp = os.path.join(outD, fn)
+    with open(inFnp) as inF:
+        with open(outFnp, 'w') as outF:
+            for idx, line in enumerate(inF):
+                toks = line.rstrip().split('\t')
+                attrs = "id:"+str(idx)
+                if 8 == len(toks):
+                    attrs += ",struct:{{thin:[],thick:[[{s},{e}]]}}".format(s=toks[6], e=toks[7])
+                out = toks[:3] + [attrs]
+                outF.write("\t".join(out) + '\n')
+    Utils.sortFile(outFnp)
+    printWroteNumLines(outFnp)
+
+    cmds = ["bgzip", '-f', outFnp]
+    Utils.runCmds(cmds)
+
+    cmds = ["tabix", '-f', outFnp + '.gz']
+    Utils.runCmds(cmds)
 
 def main():
     d = os.path.join("/home/purcarom/public_html/annotations_demo",
@@ -12,29 +37,17 @@ def main():
     outD = os.path.join(d, "washu")
     Utils.mkdir_p(outD)
 
+    jobs = []
+
     for fn in os.listdir(d):
         if not fn.endswith(".bed"):
             continue
         inFnp = os.path.join(d, fn)
-        print(inFnp)
+        jobs.append([fn, inFnp, outD])
 
-        outFnp = os.path.join(outD, fn)
-        with open(inFnp) as inF:
-            with open(outFnp, 'w') as outF:
-                for idx, line in enumerate(inF):
-                    toks = line.rstrip().split('\t')
-                    out = toks[:3] + ["id:"+str(idx)]
-                    if 8 == len(toks):
-                        out += ["struct:{thin:[],thick:[[{s},{e}]]}".format(s=toks[6], e=toks[7])]
-                    outF.write("\t".join(out) + '\n')
-        Utils.sortFile(outFnp)
-        printWroteNumLines(outFnp)
+    ret = Parallel(n_jobs = 10)(delayed(job)(j)
+                                for idx, j in enumerate(jobs))
 
-        cmds = ["bgzip", '-f', outFnp]
-        Utils.runCmds(cmds)
-
-        cmds = ["tabix", '-f', outFnp + '.gz']
-        Utils.runCmds(cmds)
 
 if __name__ == '__main__':
     main()
