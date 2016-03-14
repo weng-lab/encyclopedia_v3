dc9a07ef91eaef6a39e3925b042e9cdcbd9530b6
Michael Purcaro <purcaro@gmail.com>
2016-03-14 15:34:44 -0400

    activat buttons

--- website/views/js.js
+++ website/views/js.js
@@ -48,12 +48,14 @@ function select(devPt, checked){
         .prop('checked', checked);
 }
 
-function processFormSubmitRet(event, local_url){
+function processFormSubmitRet(event, local_url, override_data){
     event.preventDefault();
 
     $("#errBox").hide()
 
-    var formData = $("#searchForm").serializeJSON();
+    var formData = JSON.stringify(override_data) || $("#searchForm").serializeJSON();
+
+    //console.log(formData);
 
     $.ajax({
         type: "POST",
@@ -75,6 +77,57 @@ function processFormSubmitRet(event, local_url){
             var w = window.open();
             $(w.document.body).html(got["html"]);
         }
+    });
+}
+
+function selectIntersect(){
+    $("#errBox").hide()
+    $('#wait').show();
+
+    var formData = $("#searchForm").serializeJSON();
+
+    $.ajax({
+        type: "POST",
+        url: "bedsInRange",
+        data: formData,
+        dataType: "json",
+        contentType : "application/json",
+        success: function(got){
+            if("err" in got){
+                $("#errMsg").text(got["err"]);
+                $("#errBox").show()
+                return true;
+            }
+
+            if("ret" in got){
+                if(null == got["ret"]){
+                    $("#errMsg").text("not found");
+                    $("#errBox").show()
+                    return true;
+                }
+            }
+
+            var assembly = activeAssembly()
+            $.each(["Both", "H3K27ac", "DNase"], function(idx, assays) {
+                var section = $("#content" + assembly + assays);
+                section.find(":checkbox").prop('checked', false);
+
+                $.each(got["ret"][assays], function(idx, web_id){
+                    section.find(":checkbox")
+                        .filter(function(){
+                            return this.value == web_id})
+                        .prop('checked', true);
+                });
+            });
+            if(0 == got["ret"]["total"]){
+                $("#errMsg").text("No intersecting peaks found");
+                $("#errBox").show()
+            }
+        }
+    }).always(function(){
+        $('#wait').fadeOut( "slow", function() {
+            // Animation complete.
+        });
     });
 }
 
@@ -125,54 +178,32 @@ $(document).ready(function(){
     });
 
     $("#selectIntersect").click(function() {
-        $("#errBox").hide()
-        $('#wait').show();
+        selectIntersect();
+    });
 
-        var formData = $("#searchForm").serializeJSON();
+    $("#firstGalleryPane").click(function(event){
+        // hg19 at
+        var fd = {"assembly":"hg19",
+                  "loci":"rs11742570",
+                  "assays":"Both",
+                  "hg19Both": ["primary_natural_killer_cells_from_peripheral_blood_select",
+                               "primary_t_cells_from_peripheral_blood_select",
+                               "fetal_thymus_select"]
+                 };
+        processFormSubmitRet(event, "ucsc", fd);
+    });
 
-        $.ajax({
-            type: "POST",
-            url: "bedsInRange",
-            data: formData,
-            dataType: "json",
-            contentType : "application/json",
-            success: function(got){
-                if("err" in got){
-                    $("#errMsg").text(got["err"]);
-                    $("#errBox").show()
-                    return true;
-                }
-
-                if("ret" in got){
-                    if(null == got["ret"]){
-                        $("#errMsg").text("not found");
-                        $("#errBox").show()
-                        return true;
-                    }
-                }
-
-                var assembly = activeAssembly()
-                $.each(["Both", "H3K27ac", "DNase"], function(idx, assays) {
-                    var section = $("#content" + assembly + assays);
-                    section.find(":checkbox").prop('checked', false);
-
-                    $.each(got["ret"][assays], function(idx, web_id){
-                        section.find(":checkbox")
-                            .filter(function(){
-                                return this.value == web_id})
-                            .prop('checked', true);
-                    });
-                });
-                if(0 == got["ret"]["total"]){
-                    $("#errMsg").text("No intersecting peaks found");
-                    $("#errBox").show()
-                }
-            }
-        }).always(function(){
-            $('#wait').fadeOut( "slow", function() {
-                // Animation complete.
-            });
-        });
+    $("#secondGalleryPane").click(function(event){
+        // mm10 at
+        var fd = {"assembly":"mm10",
+                  "loci":"rs27106747",
+                  "assays":"Both",
+                  "mm10Both":["hindbrain_embryonic_11_5_day",
+                              "midbrain_embryonic_11_5_day",
+                              "neural_tube_embryonic_11_5_day",
+                              "limb_embryonic_11_5_day"]
+                 };
+        processFormSubmitRet(event, "washu", fd);
     });
 
 })
