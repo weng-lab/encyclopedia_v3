b1e4957038c8630cdc5386aa209818307efa5023
Michael Purcaro <purcaro@gmail.com>
2016-05-02 23:24:36 -0400

    rotate and crop

--- website/models/hic/hic.py
+++ website/models/hic/hic.py
@@ -1,29 +1,66 @@
 #!/usr/bin/env python
 
+import sys, os
 import h5py
 import numpy as np
 
-fnp = "/home/mjp/hic-Dekker/hdf5/a549/ENCODE3-A549-HindIII__hg19__hdf/C-10000000/iced/ENCODE3-A549-HindIII__hg19__genome__C-10000000-iced.hdf5"
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../metadata/utils'))
+from utils import Utils
 
-f = h5py.File(fnp, "r")
+def makeImg(fnp, chrom, start, numBins, res):
+    fnp = "/home/mjp/hic-Dekker/hdf5/a549/ENCODE3-A549-HindIII__hg19__hdf/C-40000/iced/ENCODE3-A549-HindIII__hg19__genome__C-40000-iced.hdf5"
 
-print f.keys()
-chrs = f["chrs"]
-chr_bin_range = f["chr_bin_range"]
+    f = h5py.File(fnp, "r")
 
-im = f["interactions"]
+    #print f.keys()
+    chrs = dict((c, i) for i, c in enumerate(f["chrs"]))
+    chromIdx = chrs[chrom]
 
-print im.shape
+    chr_bin_range = f["chr_bin_range"][chromIdx]
+    binIdx = start / res
+    idx = chr_bin_range[0] + binIdx
 
-for idx, c in enumerate(chrs):
-    cbr = chr_bin_range[idx]
-    #print c, ",".join([str(x) for x in im[cbr[0] : cbr[1]]])
+    im = f["interactions"]
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-m = np.matrix(im).clip(0, 1)
+    pngFnp = fnp + ".png"
+    saveMatrix(pngFnp, im[idx : idx + numBins, idx : idx + numBins])
+    rotate(pngFnp)
 
-plt.figure(1)
-plt.imshow(m, interpolation='nearest')
-plt.savefig(fnp + ".png")
+def saveMatrix(fnp, im):
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    m = np.matrix(im).clip(0, 1)
+    m = np.triu(m)
+    m[m == 0.0] = np.nan
+
+    plt.figure(1)
+    plt.imshow(m, interpolation='nearest')
+    plt.axis('off')
+    plt.savefig(fnp)
+
+def rotate(fnp):
+    from PIL import Image
+
+    s = Image.open(fnp)
+    s = s.convert('RGBA')
+    s = s.rotate(45, expand=1)
+
+    # http://stackoverflow.com/a/5253554
+    # a white image same size as rotated image
+    fff = Image.new('RGBA', s.size, (255,)*4)
+
+    # create a composite image using the alpha layer of rot as a mask
+    out = Image.composite(s, fff, s)
+
+    # save your work (converting back to mode='1' or whatever..)
+    out.convert(s.mode).save(fnp)
+
+    cmds = ["convert",
+            fnp,
+            "-trim +repage",
+            fnp + ".trim.png"]
+    Utils.runCmds(cmds)
+
+
+makeImg("", "chr1", 40000, 2000, 40000)
