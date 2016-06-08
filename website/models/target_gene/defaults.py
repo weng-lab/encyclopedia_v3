296a7b0b374307aa8a7d481af602e5cd2db5434e
Michael Purcaro <purcaro@gmail.com>
2016-06-08 09:37:42 -0400

    fiddle default

--- website/models/target_gene/defaults.py
+++ website/models/target_gene/defaults.py
@@ -4,8 +4,8 @@ class Defaults:
     def __init__(self):
         self.defaults = {}
         self.defaults["hg19"] = {}
-        self.defaults["hg19"]["pos"] = "chr14:35792756-35819812"
-        self.defaults["hg19"]["snp"] = "rs11742570"
+        self.defaults["hg19"]["pos"] = "chr4:83910641-84311040"
+        self.defaults["hg19"]["snp"] = "rs149273678"
 
         self.defaults["mm10"] = {}
         self.defaults["mm10"]["pos"] = "chr1:134054000-134071000"
